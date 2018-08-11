853c4ffadd9bc15ed49d4127ea4c1014b72a60e6
Farhan Reagan <farhan.ahmed.1@stonybrook.edu>
2018-08-11 13:14:23 -0400

    created gui

--- app/src/main/java/com/boys/bachmair/interviewbotapp/MainActivity.java
+++ app/src/main/java/com/boys/bachmair/interviewbotapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.boys.bachmair.interviewbotapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createDropdown();
+    }
+
+    protected void createDropdown() {
+        Spinner dropdown = findViewById(R.id.spinner1);
+        String[] categories = {"Basic", "Behavioral", "Salary", "Brainteaser"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categories);
+        dropdown.setAdapter(adapter);
     }
 }
